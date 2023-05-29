64873ce1ed90b3310f506a058dd38f8029802e56
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-05-29 14:49:47 -0400

    Adjustment for the api naming

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
+    axios.get('http://localhost:5000/api/activities/GetActivities').then(response => {
       setActivities(response.data);
     })
   }, []) // To only toggle once the useEffect, we need to add dependency with the []
