0cd667f1b7c9d610f25efe69bd1c596add70d9d1
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-06-06 17:02:11 -0400

    end of part 73 (Selected an Activity using MobX)

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,54 +3,38 @@ import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
 interface Props {
     activities: Activity[];
-    selectedActivity: Activity | undefined
-    selectActivity: (id: string) => void; // Here we add function. So name: (param: paramType) => returningType
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
 // export default function ActivityDashboard(props : Props) { // Same as the next line but we need to use props.activities.map....
-    export default function ActivityDashboard({activities, 
-                                                selectedActivity, 
-                                                    selectActivity, 
-                                                        cancelSelectActivity, 
-                                                            editMode, 
-                                                                openForm, 
-                                                                    closeForm,
-                                                                        createOrEdit,
-                                                                            deleteActivity,
-                                                                                submitting} : Props) {
+    export default observer (function ActivityDashboard({activities, createOrEdit, deleteActivity, submitting} : Props) 
+    {
+        const { activityStore } = useStore();
+        const { selectedActivity, editMode } = activityStore;                               
+        
         return (
             <Grid width='25%'>
                 <ActivityList activities={activities} 
-                    selectActivity={selectActivity} 
                     deleteActivity={deleteActivity}
                     submitting={submitting}
                 />
                 <Grid.Column width='6'>
                     {/* The && tells to execute only if the passed argument isn't null or undefined. Otherwise */}
                     {selectedActivity && !editMode && 
-                    <ActivityDetails 
-                        activity={selectedActivity} 
-                        cancelSelectActivity={cancelSelectActivity}
-                        openForm={openForm}
-                    />}
+                    <ActivityDetails />}
                     {editMode &&
                     <ActivityForm
-                        closeForm={closeForm}
-                        activity={selectedActivity}
                         createOrEdit={createOrEdit}
                         submitting={submitting}
                     />}
                 </Grid.Column>
             </Grid>
         )
-}
+})
