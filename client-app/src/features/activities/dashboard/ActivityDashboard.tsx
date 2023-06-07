c2932af246066abfb7ac749502f6c240e0a441d0
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-06-07 09:19:23 -0400

    Installing react router, adding routes, adding nav links

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,8 @@ import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 // export default function ActivityDashboard(props : Props) { // Same as the next line but we need to use props.activities.map....
     export default observer (function ActivityDashboard() 
@@ -11,6 +13,12 @@ import { observer } from "mobx-react-lite";
         const { activityStore } = useStore();
         const { selectedActivity, editMode } = activityStore;                               
         
+        useEffect(() => { 
+          activityStore.loadActivities();
+        }, [activityStore])
+      
+        if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+
         return (
             <Grid width='25%'>
                 <ActivityList />
