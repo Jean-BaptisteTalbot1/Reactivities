c1e8be80d35fd6b53754762b4f6111eb68d7081f
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-06-07 05:33:15 -0400

    delete an activity with mobx

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,12 @@
 import React, { SyntheticEvent, useState } from "react";
-import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props {
-    activities : Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
-export default function ActivityList({activities, deleteActivity, submitting}: Props) {
+export default observer (function ActivityList() {
+    
+    const {activityStore} = useStore();
+    const {deleteActivity, activities, loading} = activityStore;
     
     const [target, setTarget] = useState('');
 
@@ -17,9 +14,6 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
-
-    const {activityStore} = useStore();
-
 
     return (
         <Segment style={{width: '60%'}}>
@@ -37,7 +31,7 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
                                 <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue"/>
                                 <Button 
                                     name={activity.id}
-                                    loading={submitting && target === activity.id} 
+                                    loading={loading && target === activity.id} 
                                     onClick={(e) => handleActivityDelete(e, activity.id)} 
                                     floated="right" 
                                     content="Delete" 
@@ -49,4 +43,4 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
                 ))}
             </Item.Group>
         </Segment>)
-}
+})
