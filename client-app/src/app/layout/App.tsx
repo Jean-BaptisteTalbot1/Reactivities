0a8e27a5f193b022edb34ed2d5142e8df7fcb7f5
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-06-07 05:16:10 -0400

    create and edit an activity with mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,7 +3,6 @@ import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
@@ -19,31 +18,6 @@ function App()
     activityStore.loadActivities();
   }, [activityStore])
 
-  // Checks if there is an existing activity (if the passed activity has an id, it exists, else it is created)
-  // And after that, the edit mode is disabled and the new activity is selected
-  function handleCreateOrEditActivity(activity: Activity)
-  {
-      setSubmitting(true);
-
-      if (activity.id)
-      {
-        agent.Activities.update(activity).then(() => { 
-          setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
-          activityStore.selectActivity(activity.id);
-          setSubmitting(false);
-        })
-      } 
-      else 
-      {
-        activity.id = uuid();
-        agent.Activities.create(activity).then(() => { 
-          setActivities([...activities, activity]); 
-          activityStore.selectActivity(activity.id);
-          setSubmitting(false);
-        })
-      }
-  }
- 
   function handleDeleteActivity(id: string)
   {
     setSubmitting(true);
@@ -62,7 +36,6 @@ function App()
       <Container style={{marginTop: '7em'}}>
         <ActivityDashboard 
           activities={activityStore.activities}
-          createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
         />
