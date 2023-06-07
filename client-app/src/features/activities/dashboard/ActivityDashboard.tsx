086b570d88ba57d23089c8d5ecacb599775261b6
NUMOVE\jbtalbot <jbtalbot@numovegroup.com>
2023-06-07 13:31:30 -0400

    Using route parameters

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,34 +1,30 @@
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-// export default function ActivityDashboard(props : Props) { // Same as the next line but we need to use props.activities.map....
-    export default observer (function ActivityDashboard() 
-    {
-        const { activityStore } = useStore();
-        const { selectedActivity, editMode } = activityStore;                               
-        
-        useEffect(() => { 
-          activityStore.loadActivities();
-        }, [activityStore])
-      
-        if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+export default observer (function ActivityDashboard() 
+{
+    const { activityStore } = useStore();
+    
+    useEffect(() =>         { 
+        activityStore.loadActivities();
+    }, [activityStore])
+    
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
-        return (
-            <Grid width='25%'>
+    return (
+        <Grid width='25%'>
+            {/* First column */}
+            <Grid.Column width='11'>
                 <ActivityList />
-                <Grid.Column width='6'>
-                    {/* The && tells to execute only if the passed argument isn't null or undefined. Otherwise */}
-                    {selectedActivity && !editMode && 
-                    <ActivityDetails />}
-                    {editMode &&
-                    <ActivityForm />}
-                </Grid.Column>
-            </Grid>
-        )
+            </Grid.Column>
+            {/* Second column */}
+            <Grid.Column width='5'>
+                <h2>Activities filter</h2>
+            </Grid.Column>
+        </Grid>
+    )
 })
